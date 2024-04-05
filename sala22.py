caea91b2a35264d73a6082cfdcaaa1ed620c84ba
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2024-04-05 13:36:50 -0300

    Update sala22.py

---
+++ sala22.py
@@ -0,0 +1,41 @@
+import telebot
+import time
+import datetime
+import random
+
+CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" # BOT FOX
+
+bot = telebot.TeleBot(CHAVE_API)
+
+group_id = '-1001922219337'
+
+sticker_file_id = 'CAACAgEAAxkBAAMCZSbmh4EopfmSJgx8Z8sDxkeWf1UAAvwAAzgOghFAju2fQymOBzAE'
+
+mensagem = """
+🚨 <b>ENTRADA CONFIRMADA</b> 🚨
+
+🐯 Fortune Tiger 
+⏰ Estratégia: Horários Pagantes
+⚠️ Válido ate: {}
+
+💰 {}x Normal
+💰 {}x Turbo
+
+⚡ Intercalando
+
+<a href="https://bsbet.online?c=bonus">🔗 Fazer CADASTRO ✅</a>
+"""
+
+print("========")
+
+ 
+n_jogadas = random.randint(6, 20)
+n_jogadas2 = random.randint(4, 20)
+validade = datetime.datetime.now() + datetime.timedelta(minutes=10)
+hora_validade = validade.strftime("%H:%M")
+mensagem_formatada = mensagem.format(hora_validade,n_jogadas, n_jogadas2)
+bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
+
+time.sleep(120)
+bot.send_sticker(chat_id=group_id, sticker=sticker_file_id)
+time.sleep(480)
